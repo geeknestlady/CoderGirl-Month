5a517a15da9542a94936d20678f35e73e405cf64
carrieheyen@gmail.com <carrieheyen@gmail.com>
2019-02-17 15:14:40 -0600

    Homework Finish

--- Month/Program.cs
+++ Month/Program.cs
@@ -8,10 +8,60 @@ namespace Month
         {
             Console.Write("Enter the number of the month: ");
 
-            // Save the input to a variable.
-
-            // Use the if statment to update the monthName variable based on the user's input.
+            string monthInput = Console.ReadLine();
+            monthInput = monthInput.Trim();
             string monthName = string.Empty;
+
+            if(monthInput == "1"){
+                monthName = "January";
+            }
+            if(monthInput == "2") {
+                monthName = "February";
+            }
+            if(monthInput == "3")
+            {
+                monthName = "March";
+            }
+            if(monthInput == "4")
+            {
+                monthName = "April";
+            }    
+            if(monthInput == "5")
+            {
+                monthName = "May";
+            }  
+            if(monthInput == "6")
+            {
+                monthName = "June";
+            }
+            if(monthInput == "7")
+            {
+                monthName = "July";
+            }
+            if(monthInput == "8")
+            {
+                monthName = "August";
+            }
+            if(monthInput == "9")
+            {
+                monthName = "September";
+            }
+            if(monthInput == "10")
+            {
+                monthName = "October";
+            }
+            if(monthInput == "11")
+            {
+                monthName = "November";
+            }
+            if (monthInput == "12")
+            {
+                monthName = "December";
+            }
+            //else Console.WriteLine($"I'm sorry, {monthInput} is an invalid entry.");
+             
+            // Use the if statment to update the monthName variable based on the user's input.
+            
 
 
             Console.WriteLine($"The name of the month you entered is {monthName}");
